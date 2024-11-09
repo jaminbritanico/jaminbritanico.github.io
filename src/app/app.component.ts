6b9395601011106092facb9357dd9410b7ff1cfb
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-11-09 21:26:08 +0800

    cleanup app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { EducationComponent } from './features/educations/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HomeComponent, EducationComponent],  // Import RouterOutlet and other components here
+  template: `
+  <home></home>
+  <education></education>
+  `,
+  styleUrls: ['./app.style.css'],
 })
 export class AppComponent {
   title = 'jaminbritanico-portfolio';
