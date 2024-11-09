c9eda7e880bbedd1dda1788e949c8d785f02247f
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-11-09 21:25:43 +0800

    Add education component

---
+++ src/app/features/educations/education.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'education',
+  standalone: true,
+  templateUrl: './education.component.html',  // Correct path to the template file
+  styleUrls: ['./education.style.css'],       // Adjust this to your style file if needed
+  encapsulation: ViewEncapsulation.None
+})
+export class EducationComponent {}
