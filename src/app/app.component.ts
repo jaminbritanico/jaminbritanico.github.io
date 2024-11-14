8507355e85c911b8e456f4a05d235b53a0fb0518
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-11-14 22:01:19 +0800

    Add experiences page to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { ExperienceComponent } from './features/experiences/experience.component';
 import { EducationComponent } from './features/educations/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, EducationComponent],  // Import RouterOutlet and other components here
+  imports: [HomeComponent,ExperienceComponent, EducationComponent],  // Import RouterOutlet and other components here
   template: `
   <home></home>
+  <app-experience></app-experience>
   <app-education></app-education>
   `,
   styleUrls: ['./app.style.css'],
