12885e0bd2f81afd6caf16c0a90b3acd50cd5ca9
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-11-22 21:53:38 +0800

    setup loading module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ExperienceComponent } from './features/experiences/experience.component';
 import { EducationComponent } from './features/educations/education.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,ExperienceComponent, EducationComponent],  // Import RouterOutlet and other components here
+  imports: [CommonModule, LoadingComponent, HomeComponent, ExperienceComponent, EducationComponent],  // Import RouterOutlet and other components here
   template: `
-  <home></home>
-  <app-experience></app-experience>
-  <app-education></app-education>
+  <div *ngIf="isLoading; else mainContent">
+    <app-loading></app-loading>
+  </div>
+  <ng-template #mainContent>
+    <home></home>
+    <app-experience></app-experience>
+    <app-education></app-education>
+  </ng-template>
   `,
   styleUrls: ['./app.style.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jaminbritanico-portfolio';
+
+  isLoading = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 5000); // Display loading for 5 seconds
+  }
 }
