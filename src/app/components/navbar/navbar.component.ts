915a487ba4d716e1464017ef56cb331ae0e24608
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-11-27 22:40:54 +0800

    add navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.style.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NavBarComponent {}
