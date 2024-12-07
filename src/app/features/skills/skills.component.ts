972088913144a24620400e46e24546870ca7977a
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-12-07 23:13:20 +0800

    add skills component

---
+++ src/app/features/skills/skills.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  standalone: true,
+  templateUrl: './skills.component.html', 
+  styleUrls: ['./skills.style.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SkillComponent {}
