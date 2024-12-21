942238c019d1478031c60bf6501b6f743c7d5a60
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-12-21 19:49:13 +0800

    add about and project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from './components/loading/loading.component';
 import { NavBarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
 import { SkillComponent } from './features/skills/skills.component';
 import { ExperienceComponent } from './features/experiences/experience.component';
+import { ProjectComponent } from './features/project/project.component';
 import { EducationComponent } from './features/educations/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, LoadingComponent, NavBarComponent, 
-    HomeComponent, SkillComponent, ExperienceComponent, EducationComponent],
+    HomeComponent, AboutComponent, SkillComponent, ExperienceComponent, ProjectComponent, EducationComponent],
   template: `
   <div *ngIf="isLoading; else mainContent">
     <app-loading></app-loading>
@@ -20,8 +22,10 @@ import { EducationComponent } from './features/educations/education.component';
   <ng-template #mainContent>
     <app-navbar></app-navbar>
     <app-home></app-home>
+    <app-about></app-about>
     <app-skill></app-skill>
     <app-experience></app-experience>
+    <app-project></app-project>
     <app-education></app-education>
   </ng-template>
   `,
@@ -30,11 +34,11 @@ import { EducationComponent } from './features/educations/education.component';
 export class AppComponent implements OnInit {
   title = 'jaminbritanico-portfolio';
 
-  isLoading = true;
+  isLoading = false;
 
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = false;
-    }, 4000); // Display loading for 5 seconds
+    }, 4000);
   }
 }
