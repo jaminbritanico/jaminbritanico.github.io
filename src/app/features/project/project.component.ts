c2422c1baaa2db498fb0219c361234007e47d700
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2025-01-01 22:04:38 +0800

    add project ts

---
+++ src/app/features/project/project.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  standalone: true,
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.style.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ProjectComponent {
+}
