d2f9678cc49a353adb1348aa41ab70fbef46080d
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2025-01-19 01:26:15 +0800

    add home ts

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.style.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class HomeComponent {}
