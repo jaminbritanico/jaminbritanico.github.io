ab011d29cc8b9eabd9fca6268820c7c1e957608e
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2025-02-08 11:29:00 +0800

    about ts

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonComponent } from '../../components/button-download/button.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
   styleUrls: ['./about.style.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  imports: [ButtonComponent]
 })
 export class AboutComponent {
   img: string = 'assets/images/developer.png';
