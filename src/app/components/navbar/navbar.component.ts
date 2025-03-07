c9297feef832867f47279e0b43913bbe74013b5c
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2025-03-07 22:41:31 +0800

    enhance navbar ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DevModeComponent } from '../../components/dev-mode/dev-mode.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.style.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  imports: [CommonModule, DevModeComponent]
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  displayDevMode = false;
+  backToHome() {
+    window.location.href = "#home";
+  }
+  changeTheme() {
+    const theme = localStorage.getItem('theme') || 'default';
+    if(theme == "default")
+      localStorage.setItem('theme', "dark-theme");
+    else
+      localStorage.setItem('theme', "default");
+    document.body.className = theme;
+  }
+}
