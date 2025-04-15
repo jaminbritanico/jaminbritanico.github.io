e7dc4a5d16bf1d6c393c4accee2e1a46b987b713
Jamin Britanico <jaminbritanico22@gmail.com>
2025-04-15 20:58:48 +0800

    add default theme

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,15 +12,23 @@ import { DevModeComponent } from '../../components/dev-mode/dev-mode.component';
 })
 export class NavBarComponent {
   displayDevMode = false;
+  iconPath = 'assets/svg/moon.svg';
+  
   backToHome() {
     window.location.href = "#home";
   }
   changeTheme() {
     const theme = localStorage.getItem('theme') || 'default';
     if(theme == "default")
+    {
       localStorage.setItem('theme', "dark-theme");
+      this.iconPath = 'assets/svg/moon.svg';
+    }
     else
+    {
       localStorage.setItem('theme', "default");
+      this.iconPath = 'assets/svg/sun.svg';
+    }
     document.body.className = theme;
   }
 }
